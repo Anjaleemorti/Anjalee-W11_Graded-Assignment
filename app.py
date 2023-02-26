255c26274bdb7d8ba2ac0fb45d6403bcdaa9a043
Anjaleemorti <126410748+Anjaleemorti@users.noreply.github.com>
2023-02-26 22:46:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,57 @@
-# Local imports
-import datetime
 
-# Third part imports
-from flask import request
+
+### Import Libraries
+
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import pickle
 
-from ms import app
-from ms.functions import get_model_response
+warnings.filterwarnings('ignore')
 
+### Import Datset
+df = pd.read_csv("data_breast-cancer-wiscons.csv")
+# we change the class values (at the column number 2) from B to 0 and from M to 1
+df.iloc[:,1].replace('B', 0,inplace=True)
+df.iloc[:,1].replace('M', 1,inplace=True)
 
-model_name = "Breast Cancer Wisconsin (Diagnostic)"
-model_file = 'model_binary.dat.gz'
-version = "v1.0.0"
+### Splitting Data
 
+X = df[['texture_mean','area_mean','concavity_mean','area_se','concavity_se','fractal_dimension_se','smoothness_worst','concavity_worst', 'symmetry_worst','fractal_dimension_worst']]
+y = df['diagnosis']
 
-@app.route('/info', methods=['GET'])
-def info():
-    """Return model information, version, how to call"""
-    result = {}
+from sklearn.model_selection import train_test_split
 
-    result["name"] = model_name
-    result["version"] = version
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=0)
 
-    return result
+#### Data Preprocessing
 
+from sklearn.preprocessing import StandardScaler
 
-@app.route('/health', methods=['GET'])
-def health():
-    """Return service health"""
-    return 'ok'
+scaler = StandardScaler()
 
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    feature_dict = request.get_json()
-    if not feature_dict:
-        return {
-            'error': 'Body is empty.'
-        }, 500
-
-    try:
-        response = get_model_response(feature_dict)
-    except ValueError as e:
-        return {'error': str(e).split('\n')[-1].strip()}, 500
-
-    return response, 200
+x_train = scaler.fit_transform(X_train)
+x_test = scaler.transform(X_test)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+##
+from sklearn.linear_model import LogisticRegression
+clf_lr = LogisticRegression()
+clf_lr.fit(x_train, y_train)
+predictions = clf_lr.predict(x_test)
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+print("Confusion Matrix : \n\n" , confusion_matrix(predictions,y_test))
+
+print("Classification Report : \n\n" , classification_report(predictions,y_test),"\n")
+
+
+pickle.dump(clf_lr, open('model.pkl', 'wb'))
+pickle.dump(scaler, open('scaler.pkl', 'wb'))
+
+model = pickle.load(open('model.pkl', 'rb'))
+print(model)
